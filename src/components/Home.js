5b9e7b444dba3aaa91739d1ca834ab6d98c8bb7f
Anjali Chaturvedi <anjaliichaturvedi@gmail.com>
2021-05-19 17:44:37 +0530

    home + projects

---
+++ src/components/Home.js
@@ -0,0 +1,36 @@
+import React from 'react';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import Projects from './components/Projects.js';
+import { Container, Row, Col } from 'react-bootstrap';
+import profile from './profile.png';
+import { useHistory } from 'react-router-dom';
+
+const Home = (props) =>  {
+  const history = useHistory();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Container>
+          <Row>
+          <Col>
+          <img src={profile} classname="profile" alt="Profile Pic" />
+          </Col>
+          <Col>
+          <div class="mainpage">
+          <h1> Hello </h1>
+          <h3> A Bit About Me </h3>
+          <p> I'm a paragraph. Click here to add your own text and edit me.
+          I’m a great place for you to tell a story and let your users know a little more about you. </p>
+          <button class="main-button resume-btn"> Resume </button>
+          <button onClick={() => history.push('/Projects.js')} class="main-button project-btn"> Projects </button>
+          <button class="main-button contact-btn"> Contact </button>
+          </div>
+          </Col>
+          </Row>
+        </Container>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import proj1 from './proj1.jpg';
+import proj2 from './proj2.jpg';
+import proj3 from './proj3.jpg';
 
 class Projects extends Component {
     render() {
         return (
-            <div>
-                
+            <div class="Projects">
+                <h1>Projects</h1>
+                <div class="proj-row">
+                   <Container>
+                       <Row>
+                          <Col>
+                          <h2> Project 1 </h2>
+                          <p> I'm a paragraph. Click here to add your own text and edit me. It’s easy.
+                          Just click “Edit Text” or double click me to add your own content and make changes
+                          to the font. I’m a great place for you to tell a story and let your users know a
+                          little more about you. </p>
+                          </Col>
+                          <Col>
+                          <img src={proj1} classname="proj-img" alt="project-1" />
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Col>
+                          <h2> Project 2 </h2>
+                          <p> I'm a paragraph. Click here to add your own text and edit me. It’s easy.
+                          Just click “Edit Text” or double click me to add your own content and make changes
+                          to the font. I’m a great place for you to tell a story and let your users know a
+                          little more about you. </p>
+                          </Col>
+                          <Col>
+                          <img src={proj2} classname="proj-img" alt="project-2" />
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Col>
+                          <h2> Project 3 </h2>
+                          <p> I'm a paragraph. Click here to add your own text and edit me. It’s easy.
+                          Just click “Edit Text” or double click me to add your own content and make changes
+                          to the font. I’m a great place for you to tell a story and let your users know a
+                          little more about you. </p>
+                          </Col>
+                          <Col>
+                          <img src={proj3} classname="proj-img" alt="project-3" />
+                          </Col>
+                       </Row>
+                   </Container>
+                   </div>
             </div>
         )
     }
