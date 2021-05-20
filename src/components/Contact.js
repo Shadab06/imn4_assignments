15083828a397202576c428f7a5ba31b1828a1932
shataxi <dubey.shataxi3010@gmail.com>
2021-05-20 21:32:12 +0530

    Contact+reload

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,11 +10,12 @@ const Contact = () => {
     subject: "",
     message: "",
   });
+  const [emptyField, setShow] = useState(false);
 
-  const inputHandler = async (event) => {
+  const inputHandler = (event) => {
     console.log("Previous ", userDetails);
     console.log(`target value${event.target.value}`);
-    await setDetails({
+    setDetails({
       ...userDetails,
       [event.target.name]: event.target.value,
     });
@@ -22,26 +23,44 @@ const Contact = () => {
   };
 
   const submitHandler = (event) => {
-    console.log("submit");
-    event.preventDefault();
-    let template = {
-      to_name: userDetails.email,
-      from_name: userDetails.email,
-      reply_to: "",
-      message: JSON.stringify(userDetails),
-    };
-    send(
-      "service_qx8l1og",
-      "template_yxf3fgr",
-      template,
-      "user_U7M3PtiqewhW657jagBM3"
-    )
-      .then((response) => {
-        console.log("SUCCESS", response.status, response.text);
-      })
-      .catch((error) => {
-        console.log("FAILED", error);
-      });
+    if (
+      userDetails.firstName !== "" &&
+      userDetails.lastName !== "" &&
+      userDetails.email !== "" &&
+      userDetails.subject !== ""
+    ) {
+      setShow(false);
+      console.log("submit");
+      event.preventDefault();
+      let template = {
+        to_name:"Shataxi Dubey",
+        from_name: userDetails.firstName,
+        reply_to: "",
+        message: JSON.stringify(userDetails),
+      };
+      send(
+        "service_qx8l1og",
+        "template_yxf3fgr",
+        template,
+        "user_U7M3PtiqewhW657jagBM3"
+      )
+        .then((response) => {
+          console.log("SUCCESS", response.status, response.text);
+        })
+        .catch((error) => {
+          console.log("FAILED", error);
+        });
+    } else {
+      setShow(true);
+      console.log("fill all the fields");
+    }
+    setDetails({
+      firstName: "",
+      lastName: "",
+      email: "",
+      subject: "",
+      message: "",
+    });
   };
   return (
     <div className="container">
@@ -61,24 +80,31 @@ const Contact = () => {
         </div>
         <div className="col-6 gy-5">
           <div className="row">
+            {emptyField && (
+              <p className="d-flex justify-content-center bg-warning">
+                Fill the starred fields
+              </p>
+            )}
             <div className="col-3 p-0 mx-5">
               <label htmlFor="firstName " className=" float-left">
-                First Name
+                First Name*
               </label>
               <br></br>
               <input
                 type="text"
+                value={userDetails.firstName}
                 name="firstName"
                 onChange={inputHandler}
               ></input>
             </div>
             <div className="col-3 p-0 mx-5">
               <label htmlFor="lastName" className="float-left">
-                Last Name
+                Last Name*
               </label>
               <br></br>
               <input
                 type="text"
+                value={userDetails.lastName}
                 name="lastName"
                 onChange={inputHandler}
               ></input>
@@ -87,22 +113,28 @@ const Contact = () => {
           <div className="row mt-4">
             <div className="col-3 p-0 mx-5">
               <label htmlFor="email" className="float-left">
-                Email
+                Email*
               </label>
               <br></br>
               <input
                 type="email"
                 name="email"
+                value={userDetails.email}
                 onChange={inputHandler}
                 className="float-left"
               ></input>
             </div>
             <div className="col-3 mx-5 p-0">
               <label htmlFor="subject" className="float-left">
-                Subject
+                Subject*
               </label>
               <br></br>
-              <input type="text" name="subject" onChange={inputHandler}></input>
+              <input
+                type="text"
+                name="subject"
+                value={userDetails.subject}
+                onChange={inputHandler}
+              ></input>
             </div>
           </div>
           <div className="row mt-3">
@@ -114,6 +146,7 @@ const Contact = () => {
               <textarea
                 type="text"
                 name="message"
+                value={userDetails.message}
                 onChange={inputHandler}
               ></textarea>
             </div>
