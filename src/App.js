9639ca6da438c87e59a55bfd3e56975755e2205e
shataxi <dubey.shataxi3010@gmail.com>
2021-05-20 10:35:49 +0530

    Contact+Navbar+Footer

--- src/App.js
+++ src/App.js
@@ -34,17 +34,4 @@ function App() {
 }
 
 export default App;
-// <header className="App-header">
-// <img src={logo} className="App-logo" alt="logo" />
-// <p>
-//   Welcome to your first assignment
-// </p>
-// <a
-//   className="App-link"
-//   href="https://www.wix.com/website-template/view/html/2846?siteId=9dabe0d0-1603-4a04-9bd6-7d9d9ca9e9cb&metaSiteId=3e8b1a18-1c30-41cf-87ac-e18816787ea8&originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fall%2F2&tpClick=view_button"
-//   target="_blank"
-//   rel="noopener noreferrer"
-// >
-//   Click me
-// </a>
-// </header>
+
