9046682e0ded33fc827150c2d558ee79f7a45416
ar30612 <ar30612@gmail.com>
2021-05-20 18:49:03 +0200

    Navbar - contact -footer

--- src/App.js
+++ src/App.js
@@ -4,31 +4,22 @@ import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import Resume from "./components/Resume";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from './components/Home';
 
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact>
-            Home Component
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/projects">
-            <Projects />
-          </Route>
-          <Route path="/resume">
-            <Resume />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-      <Footer />
+      <Navbar></Navbar>
+      <Switch>
+        <Route exact path='/' component= { Home } ></Route>
+        <Route  path='/resume' component = { Resume } ></Route>
+        <Route  path='/projects' component = { Projects } ></Route>
+        <Route  path='/contact' component = { Contact } ></Route>
+      </Switch>
+      <Footer></Footer>
     </div>
   );
 };
